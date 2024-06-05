4f25eda1c41f7db33b517ed03810602b6530888b
VN_ <spacekitty135@gmail.com>
2024-06-05 10:41:03 +0300

    feat:Improved httpservice, added simple cache

--- Assets/Scripts/Services/HttpService.cs
+++ Assets/Scripts/Services/HttpService.cs
@@ -1,47 +1,71 @@
 using UnityEngine;
 using System.Net.Http;
-//using Microsoft.Extensions.Caching.Memory;
 using System.Threading.Tasks;
 using System;
 using System.Text;
+using System.Collections.Concurrent;
+#if TEST_SLOWHTTP
+using System.Threading;
+#endif
 
 namespace GameServices
 {
     public class HttpService : IService
     {
         private HttpClient httpClient = new();
-        //private IMemoryCache _cache;
+        private readonly bool isUseCache = false;
+        private readonly int cacheTTLMinutes = 3;
+        private readonly ConcurrentDictionary<string, CacheItem> cacheDictionary = new();
 
-        public HttpService()
+        private class CacheItem
         {
-            // HttpRequestCachePolicy policy = new HttpRequestCachePolicy(HttpRequestCacheLevel.Default);
-            // HttpWebRequest.DefaultCachePolicy = policy;
+            public string Data;
+            public DateTime Expiration { get; }
+
+            public CacheItem(string data, DateTime expiration)
+            {
+                Data = data;
+                Expiration = expiration;
+            }
+        }
+
+        public HttpService(bool isUseCache = false, int timeoutSeconds = 10, int cacheTTLMinutes = 3)
+        {
+            httpClient.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
+            this.isUseCache = isUseCache;
+            this.cacheTTLMinutes = cacheTTLMinutes;
         }
 
         public async Task<string> GetRequestAsync(string url)
         {
+            if (this.isUseCache && cacheDictionary.TryGetValue(url, out CacheItem cachedItem))
+            {
+                if (cachedItem.Expiration >= DateTime.Now)
+                {
+                    return cachedItem.Data;
+                }
+                else
+                {
+                    cacheDictionary.TryRemove(url, out _);
+                }
+            }
+
             try
             {
                 var resp = await httpClient.GetAsync(url);
                 resp.EnsureSuccessStatusCode(); //Will throw an exception if status code is not 2xx
                 var result = await resp.Content.ReadAsStringAsync();
+
+                if (isUseCache)
+                {
+                    DateTime expiration = GetCacheExpireTime(resp);
+                    cacheDictionary[url] = new CacheItem(result, expiration);
+                    Debug.Log($"[HttpService] Cache expire time {expiration} ; Time now: {DateTime.Now}");
+                }
+
                 return result;
             }
             catch (Exception e)
-            {
-                Debug.LogError($"[HttpService] Request to {url} failed with an exception {e.Message}");
-                throw;
-            }
-        }
-
-        public async Task DeleteRequestAsync(string url)
-        {
-            try
-            {
-                var resp = await httpClient.DeleteAsync(url);
-                resp.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException e)
             {
                 Debug.LogError($"[HttpService] Request to {url} failed with an exception {e.Message}");
                 throw;
@@ -52,6 +76,9 @@ namespace GameServices
         {
             try
             {
+#if TEST_SLOWHTTP
+            await Task.Run(() => { Thread.Sleep(3000); });
+#endif
                 HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
                 HttpResponseMessage response = await httpClient.PostAsync(url, content);
                 response.EnsureSuccessStatusCode();
@@ -69,6 +96,9 @@ namespace GameServices
         {
             try
             {
+#if TEST_SLOWHTTP
+            await Task.Run(() => { Thread.Sleep(3000); });
+#endif
                 HttpContent content = new StringContent(dataJSON, Encoding.UTF8, "application/json");
                 HttpResponseMessage response = await httpClient.PutAsync(url, content);
                 response.EnsureSuccessStatusCode();
@@ -82,17 +112,20 @@ namespace GameServices
             }
         }
 
-        public void CancelPendingRequests()
+        private DateTime GetCacheExpireTime(HttpResponseMessage resp)
         {
-            try
+            if (resp.Headers.CacheControl?.MaxAge != null)
             {
-                this.httpClient.CancelPendingRequests();
+                Debug.Log($"MaxAge {resp.Headers.CacheControl.MaxAge.Value.TotalSeconds}");
+                return DateTime.Now.Add(resp.Headers.CacheControl.MaxAge.Value);
             }
-            catch (Exception e)
+
+            if (resp.Content.Headers.Expires != null)
             {
-                Debug.LogError($"[HttpService] Failed to cancel pending requests {e}");
-                throw;
+                return resp.Content.Headers.Expires.Value.DateTime;
             }
+
+            return DateTime.Now.AddMinutes(cacheTTLMinutes);
         }
     }
 }
